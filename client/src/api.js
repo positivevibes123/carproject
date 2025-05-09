28c8fd0f027aab4a39c6be52143243ef43b972c6
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-05-09 18:11:20 -0400

    Simplified fetch
    Fetch is simplified for now so I can debug

--- client/src/api.js
+++ client/src/api.js
@@ -1,45 +1,18 @@
-// https://stackoverflow.com/questions/69932559/how-do-i-fix-or-work-around-this-memory-leak-in-fetch
-
-/*export async function api(subDirectory) {
-    let allProducts = []
-    let page = 1
-    let keepFetching = true
-
-    while (keepFetching) {
-        const response = await fetch(`https://wheelcollectors.com/collections/${subDirectory}/products.json?limit=250&page=${page}`)
-        const data = await response.json()
-        
-        if (data.products.length > 0) {
-            allProducts.push(...data.products)
-            page++
-        } else {
-            keepFetching = false
-        }
-    }
-
-    // Filter available products - have to do this manually since CORS is enabled on the website
-    let availableProducts = allProducts.filter(product => product.variants[0].available === true)
-    return availableProducts
-}*/
-
 export async function api(subDirectory) {
   try {
-    let availableProducts = [];
-    let data = {}
+    const res = await fetch(
+      `https://wheelcollectors.com/collections/${subDirectory}/products.json?limit=1`
+    )
 
-      const res = await fetch(
-        `https://wheelcollectors.com/collections/${subDirectory}/products.json?limit=250`
-      )
+    const data = await res.json()
 
-      data = await res.json()
-      availableProducts.push(...data.products)
-
-    // Filter available products - have to do this manually since CORS is enabled on the website
-    availableProducts = availableProducts.filter(
+    const availableProducts = data.products.filter(
       (product) => product.variants[0].available === true
     )
-    return availableProducts;
+
+    return availableProducts
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error fetching data:", error)
+    return [] // Important to avoid undefined returns
   }
 }
